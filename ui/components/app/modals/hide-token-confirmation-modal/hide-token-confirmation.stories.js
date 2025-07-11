81a6701a83cc8880b95896ea718bd7f0b51f8ab6
arlindhoxhaet67 <148649205+arlindhoxhaet67@users.noreply.github.com>
2025-07-12 01:31:23 +0200

    Create hide-token-confirmation.stories.js

---
+++ ui/components/app/modals/hide-token-confirmation-modal/hide-token-confirmation.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HideTokenConfirmationModal from '.';
+
+export default {
+  title: 'Components/App/Modals/HideTokenConfirmationModal',
+};
+
+const DefaultStory = () => <HideTokenConfirmationModal />;
+
+DefaultStory.storyName = 'Default';
+
+export { DefaultStory };
