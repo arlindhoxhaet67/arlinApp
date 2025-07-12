015eb03b77afc9a923bbe0ec8e7a64ffb3ff1fe7
arlindhoxhaet67 <148649205+arlindhoxhaet67@users.noreply.github.com>
2025-07-12 08:39:06 +0200

    Create account-not-found.stories.tsx

---
+++ ui/pages/onboarding-flow/account-not-found/account-not-found.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AccountNotFound from './account-not-found';
+
+export default {
+  title: 'Pages/OnboardingFlow/AccountNotFound',
+};
+
+export const Default = () => (
+  <div style={{ maxWidth: 460, margin: 'auto' }}>
+    <AccountNotFound />
+  </div>
+);
