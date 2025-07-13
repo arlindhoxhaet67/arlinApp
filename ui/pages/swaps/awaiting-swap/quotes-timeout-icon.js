f59080f49651691244d67b1a7e8157b27a9a1b72
arlindhoxhaet67 <148649205+arlindhoxhaet67@users.noreply.github.com>
2025-07-13 11:06:24 +0200

    Create quotes-timeout-icon.js

---
+++ ui/pages/swaps/awaiting-swap/quotes-timeout-icon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function QuotesTimeoutIcon() {
+  return (
+    <svg
+      width="44"
+      height="44"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.5896e-17.7965e-3, -5.3578e-3 L .968416, -4.8731e-3 L .968416, -2.5e-3 L -9.7907e-3, .158 L -.158,. 158 L -.968,-.06L-.9,-(.)
+        " stroke="#AABBCC" strokeWidth={". (?)"}
+        fillRule='evenodd'
+        clipRule='evenodd'
+        className=""
+        id=""
+        draggable={false}
+          />
+    </svg>
+);
+}
